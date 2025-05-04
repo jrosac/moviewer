011c4487c6af25052b2173a0ec4caec699d89653
jrosac <joaorosa.conceicao@dcomp.ufs.br>
2025-05-04 16:22:07 -0300

    feat: added the api atributes to the home page

--- projeto/src/pages/Home/index.js
+++ projeto/src/pages/Home/index.js
@@ -1,10 +1,50 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import {Link} from "react-router-dom";
+import "./home.css";
 
+// URL da API: /movie/now_playing?api_key=ba68f6fb3af7683cd59f5d676ecb54be
 
 function Home(){
 
+    const [filmes, setFilmes] = useState([])
+
+    useEffect(()=>{
+        async function loadFilmes(){
+            const response = await api.get("movie/now_playing",{
+                params:{
+                    api_key: "ba68f6fb3af7683cd59f5d676ecb54be",
+                    language:"pt-BR",
+                    page:1,
+                }
+            })
+
+           //console.log(response.data.results.slice(0,10));
+           setFilmes(response.data.results.slice(0,10))
+        }
+
+        loadFilmes();
+
+    },[])
+
     return(
-        <div>
-            Página de Home
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme)=>{
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src ={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                            
+
+                        </article>
+                    )
+
+                })}
+
+            </div>
+            
         </div>
     )
 }
