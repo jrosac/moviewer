bd36f5bab773734e5a3660a92b2ea5e7034364cc
jrosac <joaorosa.conceicao@dcomp.ufs.br>
2025-05-12 09:31:02 -0300

    feat: added the loading state to the home page

--- projeto/src/pages/Home/index.js
+++ projeto/src/pages/Home/index.js
@@ -8,6 +8,7 @@ import "./home.css";
 function Home(){
 
     const [filmes, setFilmes] = useState([])
+    const [loading, setloading] = useState(true)
 
     useEffect(()=>{
         async function loadFilmes(){
@@ -21,12 +22,21 @@ function Home(){
 
            //console.log(response.data.results.slice(0,10));
            setFilmes(response.data.results.slice(0,10))
+           setloading(false);
         }
 
         loadFilmes();
 
     },[])
 
+    if(loading){
+        return(
+            <div className="loading">
+                <h2>Carregando filmes...</h2>
+            </div>
+        )
+    }
+    
     return(
         <div className="container">
             <div className="lista-filmes">
