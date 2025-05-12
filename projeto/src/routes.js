19cf34985b2e809d99230af0c8d388beb7eab8ff
jrosac <joaorosa.conceicao@dcomp.ufs.br>
2025-05-12 10:53:20 -0300

    feat: added the favoritos route

--- projeto/src/routes.js
+++ projeto/src/routes.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home"
 import Filme from "./pages/Filme"
 import Header from "./components/Header"
 import Erro from "./pages/Erro"
+import Favoritos from "./pages/Favoritos"
 
 function RoutesApp(){
 
@@ -16,6 +17,7 @@ function RoutesApp(){
         <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/filme/:id" element={<Filme/>} />
+            <Route path="/favoritos" element={<Favoritos/>}/>
             
             
             
